e7ed4200f27094a8dc6bc0de43b6694c09a37492
Alex <24egglal@gmail.com>
2025-07-03 16:06:15 -0400

    Uploaded source code as of 2.5

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -1,12 +1,24 @@
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
 difficulty_choices = (('easy', 'Easy'), ('interm', 'Intermediate'), ('medi', 'Medium'), ('hard', 'Hard'))
-class Recipe(models.Model):
-  name = models.CharField(max_length=120)
-  ingredients = models.TextField()
-  difficulty = models.CharField(max_length=12, choices= difficulty_choices, default='easy')
-  cooking_time = models.IntegerField(help_text='in minutes!')
+
+class Ingredient(models.Model):
+  name = models.CharField(max_length=120, unique=True)
 
   def __str__(self):
-    return f"Name: {self.name}, Ingredients: {self.ingredients}, Cooking Time: {self.cooking_time}, Difficulty: {self.difficulty} "
+    return self.name
+
+class Recipe(models.Model):
+  name = models.CharField(max_length=120)
+  description = models.TextField(max_length=500, default='Please describe your recipe.')
+  ingredients = models.ManyToManyField(Ingredient)
+  difficulty = models.CharField(max_length=12, choices= difficulty_choices, default='easy')
+  cooking_time = models.IntegerField(help_text='in minutes!')
+  pic = models.ImageField(upload_to='recipes', default='no_picture.jpg')
+
+  def __str__(self):
+    return f"Name: {self.name}, Ingredients: {self.ingredients.name}, Cooking Time: {self.cooking_time}, Difficulty: {self.difficulty} "
+  def get_absolute_url(self):
+    return reverse ('recipes:detail', kwargs={'pk': self.pk})
--- src/recipes/tests.py
+++ src/recipes/tests.py
@@ -1,10 +1,19 @@
 from django.test import TestCase
-from .models import Recipe
+from .models import Recipe, Ingredient
+from django.core.files.uploadedfile import SimpleUploadedFile
+
 # Create your tests here.
 
 class RecipeModelTest(TestCase):
   def setUpTestData():
-    Recipe.objects.create(name='Tea', ingredients='Tea Leaves, Water, Sugar', cooking_time = 5, difficulty='easy')
+    tea_leaves = Ingredient.objects.create(name="tea-leaves")
+    water = Ingredient.objects.create(name="water")
+    sugar = Ingredient.objects.create(name="sugar")
+    pic = SimpleUploadedFile(
+            "..\static\recipes\Images\Tea.jpg", content=b"", content_type="image/jpeg"
+        )
+    recipe = Recipe.objects.create(name='Tea',  cooking_time = 5, difficulty='easy', pic=pic)
+    recipe.ingredients.set([tea_leaves, water, sugar])
 
   def test_recipe_name(self):
     recipe = Recipe.objects.get(id=1)
@@ -25,3 +34,7 @@ class RecipeModelTest(TestCase):
     recipe = Recipe.objects.get(id=1)
     field_label = recipe._meta.get_field('difficulty').verbose_name
     self.assertEqual(field_label, 'difficulty')
+
+  def test_get_absolute_url(self):
+    recipe = Recipe.objects.get(id=1)
+    self.assertEqual(recipe.get_absolute_url(), '/list/1')
