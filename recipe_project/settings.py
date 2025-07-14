f909f3917ad1d00f3f592060d5a199e6380e0fe4
Egglol1 <24egglal@gmail.com>
2025-07-14 17:43:54 -0400

    Added heroku to ALLOWED_HOSTS

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1:8000', '127.0.0.1', 'enthousiaste-maison-78953-874d037b1b47.herokuapp.com']
 
 
 # Application definition
